bab040dd865d7fd6ed1a417f833458fad3e45944
VijayaKumar T M <vtmuni@codeaurora.org>
2016-11-24 14:17:02 +0530

    msm: sensor: Validationg function pointers before using them
    Since IOCTLS can come in any order, validating the actuator
    function table and methods before accessing them.
    
    CRs-Fixed: 1084177
    Change-Id: Ic6fce52fdf4d1420c2b707ec9bc9cba045066a13
    Signed-off-by: Sureshnaidu Laveti <lsuresh@codeaurora.org>
    Signed-off-by: VijayaKumar T M <vtmuni@codeaurora.org>
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

--- drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
+++ drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
@@ -1614,7 +1614,7 @@ static int32_t msm_actuator_config(struct msm_actuator_ctrl_t *a_ctrl,
 {
 	struct msm_actuator_cfg_data *cdata =
 		(struct msm_actuator_cfg_data *)argp;
-	int32_t rc = 0;
+	int32_t rc = -EINVAL;
 	if (!a_ctrl) {
 		pr_err("failed\n");
 		return -EINVAL;
@@ -1643,6 +1643,8 @@ static int32_t msm_actuator_config(struct msm_actuator_ctrl_t *a_ctrl,
 		break;
 
 	case CFG_MOVE_FOCUS:
+		if (a_ctrl->func_tbl &&
+			a_ctrl->func_tbl->actuator_move_focus)
 			rc = a_ctrl->func_tbl->actuator_move_focus(a_ctrl,	
 				&cdata->cfg.move);	
 		if (rc < 0)
@@ -1650,6 +1652,8 @@ static int32_t msm_actuator_config(struct msm_actuator_ctrl_t *a_ctrl,
 		break;
 
 	case CFG_SET_POSITION:
+		if (a_ctrl->func_tbl &&
+			a_ctrl->func_tbl->actuator_set_position)
 			rc = a_ctrl->func_tbl->actuator_set_position(a_ctrl,	
 				&cdata->cfg.setpos);	
 		if (rc < 0)
